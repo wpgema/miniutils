e0053ef1bd1bf587a90fe741567ee1e7d1974889
wpgema <mgemamaulana04@gmail.com>
2025-09-27 02:27:20 +0700

    Fix formatting in formatRupiah tests to include currency symbol

--- test/rupiahFormatter.test.js
+++ test/rupiahFormatter.test.js
@@ -2,15 +2,15 @@ import { formatRupiah } from "../src/number/rupiahFormatter.js";
 
 describe("formatRupiah", () => {
     test("should format integer correctly", () => {
-        expect(formatRupiah(10000)).toBe("Rp10.000,00");
+        expect(formatRupiah(10000)).toBe("Rp\u00A010.000,00");
     });
 
     test("should format decimal correctly", () => {
-        expect(formatRupiah(1234.56)).toBe("Rp1.234,56");
+        expect(formatRupiah(1234.56)).toBe("Rp\u00A01.234,56");
     });
 
     test("should format large number with separators", () => {
-        expect(formatRupiah(1000000)).toBe("Rp1.000.000,00");
+        expect(formatRupiah(1000000)).toBe("Rp\u00A01.000.000,00");
     });
 
     test("should throw error if input is not number", () => {
